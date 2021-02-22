c18d1cc301d9052ff39f41eb92390c084599b8e8
JulioCesarZbz <juliocesar_tkd@hotmail.com>
2021-02-22 18:39:24 -0300

    TextField & DataPicker

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -1,72 +1,101 @@
 package gui.util;
 
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Locale;
 
 import javafx.scene.Node;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
 public class Utils {
 
 	public static Stage currentStage(javafx.event.ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
-	
-	//converter o valor do textbox para Int
+
+	// converter o valor do textbox para Int
 	public static Integer tryParseToInt(String str) {
-		// se for lançado qualquer caractere diferente de numero, a aplicação retorna um valor nulo
+		// se for lançado qualquer caractere diferente de numero, a aplicação retorna um
+		// valor nulo
 		try {
-		return Integer.parseInt(str);
-		}
-		catch (NumberFormatException e) {
+			return Integer.parseInt(str);
+		} catch (NumberFormatException e) {
 			return null;
 		}
 	}
-	
+
 	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
 		tableColumn.setCellFactory(column -> {
-			TableCell<T, Date> cell = new TableCell<T, Date>(){
+			TableCell<T, Date> cell = new TableCell<T, Date>() {
 				private SimpleDateFormat sdf = new SimpleDateFormat(format);
-				
+
 				@Override
 				protected void updateItem(Date item, boolean empty) {
 					super.updateItem(item, empty);
-					if(empty) {
+					if (empty) {
 						setText(null);
-					}else {
+					} else {
 						setText(sdf.format(item));
 					}
 				}
 			};
 			return cell;
-		}
-		);
-		
+		});
+
 	}
-	
+
 	public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
-		tableColumn.setCellFactory(column ->{
+		tableColumn.setCellFactory(column -> {
 			TableCell<T, Double> cell = new TableCell<T, Double>() {
-				
+
 				@Override
 				protected void updateItem(Double item, boolean empty) {
 					super.updateItem(item, empty);
-					if(empty) {
+					if (empty) {
 						setText(null);
-					}else {
+					} else {
 						Locale.setDefault(Locale.US);
-						setText(String.format("%."+decimalPlaces+"f", item));
+						setText(String.format("%." + decimalPlaces + "f", item));
 					}
 				}
-				
+
 			};
 			return cell;
 		});
 	}
-	
-	
-		
+
+	public static void formatDatePicker(DatePicker datePicker, String format) {
+		datePicker.setConverter(new StringConverter<LocalDate>() {
+
+			DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(format);
+			{
+				datePicker.setPromptText(format.toLowerCase());
+			}
+
+			@Override
+			public String toString(LocalDate date) {
+				if (date != null) {
+					return dateFormatter.format(date);
+				} else {
+					return "";
+				}
+			}
+
+			@Override
+			public LocalDate fromString(String string) {
+				if (string != null && !string.isEmpty()) {
+					return LocalDate.parse(string, dateFormatter);
+				} else {
+					return null;
+				}
+			}
+		});
+	}
+
 }
