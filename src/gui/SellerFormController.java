d47e9fe58584fd99cd3b8a6497dbfd28bbdce0b8
JulioCesarZbz <juliocesar_tkd@hotmail.com>
2021-02-22 17:49:17 -0300

    Seller Form

---
+++ src/gui/SellerFormController.java
@@ -0,0 +1,153 @@
+package gui;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+import java.util.Set;
+
+import db.DbException;
+import gui.listeners.DataChangeListener;
+import gui.util.Alerts;
+import gui.util.Constraints;
+import gui.util.Utils;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import model.entities.Seller;
+import model.exceptions.ValidationException;
+import model.services.SellerService;
+
+public class SellerFormController implements Initializable {
+
+	// criando uma dependencia para o departamento
+	private Seller entity;
+	
+	// criando uma dependencia para o SellerService
+	private SellerService service;
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
+	
+	//declaração dos campos para interação com o form
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtName;
+	
+	@FXML
+	private Label labelErrorName;
+	
+	@FXML
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	//implementando o método set da entity
+	public void setSeller(Seller entity) {
+		this.entity = entity;
+	}
+	
+	//implementando o metodo set do service
+	public void setSellerService(SellerService service) {
+		this.service = service;
+	}
+	
+	public void subscribeDataChamngeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
+	
+	//Declaração dos métodos para tratar as açoes dos botões 
+	@FXML
+	public void onBtSaveActioon(ActionEvent event) {
+		if(entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entity = getFormData();
+			//salvando no banco de dados
+			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
+			//fechar a janela apos salvar
+			Utils.currentStage(event).close();
+		}
+		catch(ValidationException e) {
+			setErrormessages(e.getErrors());
+		}
+		catch(DbException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private void notifyDataChangeListeners() {
+		for (DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}
+		
+	}
+
+	private Seller getFormData() {
+		Seller obj = new Seller();
+		
+		ValidationException exception = new ValidationException("Validation Error");
+		
+		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		if(txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "Field Can't be Empty");
+		}
+		obj.setName(txtName.getText());
+		
+		if(exception.getErrors().size() > 0) {
+			throw exception;
+		}
+		
+		return obj;
+	}
+
+	@FXML
+	public void onBtCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
+	}
+
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+	//chama o controlador de restrições
+		initializeNodes();
+	}
+
+	//iniciamos inserindo restrições
+	private void initializeNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFieldMaxLength(txtName, 30);
+	}
+	
+	//popular as textbox do formulario com as informações do objeto entity
+	public void updateFormData() {
+		//programação defensiva pra testar se o entity está nulo
+		if(entity ==null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		//fim da verificação
+		txtId.setText(String.valueOf(entity.getId()));
+		txtName.setText(entity.getName());
+	}
+	
+	private void setErrormessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
+		
+		if(fields.contains("name")) {
+			labelErrorName.setText(errors.get("name"));
+		}
+	}
+}
